8795ed67c3096739a1ea6af987b82045ad2c310b
colmpat <colmpat@users.noreply.github.com>
2023-03-01 20:42:35 +0000

    deploy: 775277c4463ca543479e5ad517bc545e2aa908fd

--- data/cleanDiamonds.py
+++ data/cleanDiamonds.py
@@ -1,4 +1,5 @@
 import pandas
+import random
 
 data = pandas.read_csv("_diamonds.csv")
 
@@ -9,4 +10,8 @@ for _, row in data.iterrows():
         'carat': row.get('carat'),
     })
 
+# Shuffle the data
+random.shuffle(newData)
+
+# Save the new data to a new file
 pandas.DataFrame(newData).to_csv('diamonds.csv')
